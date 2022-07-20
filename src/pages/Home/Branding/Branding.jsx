dee0ca21283775c97f3f6c00abf420350a98dcf4
@ ANTU <sajolhowlader08@gmail.com>
2022-07-20 17:13:44 +0600

    add branding img

--- src/pages/Home/Branding/Branding.jsx
+++ src/pages/Home/Branding/Branding.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import bImgOne from "../../../assets/images/branding-company-img/1.jpg";
+import bImgTwo from "../../../assets/images/branding-company-img/2.jpg";
+import bImgThree from "../../../assets/images/branding-company-img/3.png";
+import bImgfour from "../../../assets/images/branding-company-img/4.png";
+import bImgfive from "../../../assets/images/branding-company-img/5.png";
+import bImgsix from "../../../assets/images/branding-company-img/6.png";
 
 const Branding = () => {
   return (
-    <div>
-      <h2>This is branding section</h2>
-    </div>
+    <>
+      <h1 className='bSectionTitle'>Out Trusted Clints</h1>
+      <div className="branding-container">
+        <div className="brandingImg">
+          <img src={bImgOne} alt="" />
+        </div>
+        <div className="brandingImg">
+          <img src={bImgTwo} alt="" />
+        </div>
+        <div className="brandingImg">
+          <img src={bImgThree} alt="" />
+        </div>
+        <div className="brandingImg">
+          <img src={bImgfour} alt="" />
+        </div>
+        <div className="brandingImg">
+          <img src={bImgfive} alt="" />
+        </div>
+        <div className="brandingImg">
+          <img src={bImgsix} alt="" />
+        </div>
+       
+      </div>
+    </>
   );
 };
 
