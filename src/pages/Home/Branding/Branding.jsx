cef95b83494c4c19cfc4ef7a8ab32794f7cc32f2
@ ANTU <sajolhowlader08@gmail.com>
2022-07-21 01:17:21 +0600

    add slider

--- src/pages/Home/Branding/Branding.jsx
+++ src/pages/Home/Branding/Branding.jsx
@@ -1,15 +1,55 @@
-import React from 'react';
-import './Branding.css';
+import React, { useEffect, useState } from "react";
+import { Autoplay, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./Branding.css";
 const Branding = () => {
+  const [sliderImg, setSliderImg] = useState([]);
+  useEffect(() => {
+    fetch("brandingSection.json")
+      .then((res) => res.json())
+      .then((data) => setSliderImg(data));
+  }, []);
   return (
     <>
-     <div className="titleContainer">
-     <h1 className='bSectionTitle'>Branding Section</h1>
-     <span></span>
-     </div>
-    <div className="brandingContainer">
-      
-    </div>
+      <div className="titleContainer">
+        <h1 className="bSectionTitle">Branding Section</h1>
+        <span></span>
+      </div>
+      <div className="brandingContainer">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{ delay: 1000, disableOnInteraction: false }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          {sliderImg.map((singleImg) => (
+            <SwiperSlide key={singleImg._id}>
+              <img src={singleImg.picture} alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </>
   );
 };
