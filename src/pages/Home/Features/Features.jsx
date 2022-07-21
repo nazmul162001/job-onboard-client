67e9aa1c6522383a5374c50a7fc3fa6ee96a1267
shawon-MG <work.shawomg@gmail.com>
2022-07-21 20:37:59 +0600

    Code upadated with a new design

--- src/pages/Home/Features/Features.jsx
+++ src/pages/Home/Features/Features.jsx
@@ -3,40 +3,42 @@ import React from 'react';
 const Features = () => {
   return (
 
-    <div className='grid grid-cols-on md:grid-cols-2 lg:grid-cols-4 gap-5 container mx-auto lg:px-8 pt-12 lg:pt-0'>
+    <div>
+      <h1 className='text-center text-5xl font-bold opacity-70'> What's Inside?</h1>
+      <div className="line w-32 rounded-full opacity-70 h-1 mx-auto bg-black mt-2"></div>
+      <div className='grid grid-cols-on md:grid-cols-2 lg:grid-cols-3 gap-4 container mx-auto lg:px-8 pt-12 lg:pt-0'>
 
-      <div class="card bg-base-100 shadow-xl image-full">
-        <figure> <img src="https://i.ibb.co/8x8L5Sy/Screenshot-2022-07-21-172536.png" alt="Shoes" /> </figure>
-        <div class="card-body">
-          <h2 class="card-title text-xl text-black"> ScreenShot-1 : Simple Log In </h2>
-          <p className=' text-black'>Any user with a google account can create account or, log in. </p>
+        <div class="card  bg-base-100 shadow-xl flex justify-center">
+          <div class="card-body">
+            <h2 class="text-start text-lg font-bold"> Smart Log In </h2>
+          </div>
+          <figure><img className='h-48' src="https://i.ibb.co/8x8L5Sy/Screenshot-2022-07-21-172536.png" alt="Shoes" /></figure>
         </div>
-      </div>
-      <div class="card bg-base-100 shadow-xl image-full">
-        <figure><img src="https://i.ibb.co/dQrjWRz/Screenshot-2022-07-21-173353.png" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title text-xl text-black"> ScreenShot-2 : Recruiter's Dashboard </h2>
-          <p className=' text-black'> Recruiter will post job circular and receive notifications of job apply done by applicants  </p>
+        <div class="card  bg-base-100 shadow-xl flex justify-center">
+          <div class="card-body">
+            <h2 class="text-start text-lg font-bold"> Recruiter Dashboard </h2>
+          </div>
+          <figure><img className='h-48' src="https://i.ibb.co/hYM5wb5/Screenshot-2022-07-21-173126.png" alt="Shoes" /></figure>
         </div>
-      </div>
-      <div class="card bg-base-100 shadow-xl image-full">
-        <figure> <img src="https://i.ibb.co/hYM5wb5/Screenshot-2022-07-21-173126.png" alt="Shoes" /> </figure>
-        <div class="card-body">
-          <h2 class="card-title text-xl text-black">ScreenShot-3 : More about the Dashboard !</h2>
-          <p className=' text-black'> Recruiter can also monitor all of their posts and can track , regulate all of the applicants whom will apply their circular using our stored data of the applicants.</p>
+        <div class="card bg-base-100 shadow-xl flex justify-center">
+          <div class="card-body">
+            <h2 class="text-start text-lg font-bold"> Applicant's UI </h2>
+          </div>
+          <figure><img className='h-48' src="https://i.ibb.co/Jjvszmt/Screenshot-2022-07-21-173712.png" alt="Shoes" /></figure>
         </div>
-      </div>
-      <div class="card bg-base-100 shadow-xl image-full">
-        <figure><img src="https://i.ibb.co/Jjvszmt/Screenshot-2022-07-21-173712.png" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title text-xl text-black">ScreenShot-4 : Explore Section </h2>
-          <p className=' text-black'> Applicant's will have a special page for seeing all job posts. </p>
-        </div>
-      </div>
 
+      </div>
 
     </div>
+
+
+
   );
 };
 
 export default Features;
+
+// https://i.ibb.co/8x8L5Sy/Screenshot-2022-07-21-172536.png
+// https://i.ibb.co/dQrjWRz/Screenshot-2022-07-21-173353.png
+// https://i.ibb.co/hYM5wb5/Screenshot-2022-07-21-173126.png
+// https://i.ibb.co/Jjvszmt/Screenshot-2022-07-21-173712.png
