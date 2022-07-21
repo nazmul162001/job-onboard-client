1d100a167525671dc9626c7ef929faf5f79466f7
shawon-MG <work.shawomg@gmail.com>
2022-07-21 17:51:15 +0600

    text color updated

--- src/pages/Home/Features/Features.jsx
+++ src/pages/Home/Features/Features.jsx
@@ -8,36 +8,34 @@ const Features = () => {
       <div class="card bg-base-100 shadow-xl image-full">
         <figure> <img src="https://i.ibb.co/8x8L5Sy/Screenshot-2022-07-21-172536.png" alt="Shoes" /> </figure>
         <div class="card-body">
-          <h2 class="card-title"> ScreenShot-1 : Simple Log In </h2>
-          <p>Any user with a google account can create account or, log in. </p>
+          <h2 class="card-title text-xl text-black"> ScreenShot-1 : Simple Log In </h2>
+          <p className=' text-black'>Any user with a google account can create account or, log in. </p>
         </div>
       </div>
       <div class="card bg-base-100 shadow-xl image-full">
         <figure><img src="https://i.ibb.co/dQrjWRz/Screenshot-2022-07-21-173353.png" alt="Shoes" /></figure>
         <div class="card-body">
-          <h2 class="card-title"> ScreenShot-2 : Recruiter's Dashboard </h2>
-          <p> Recruiter will post job circular and receive notifications of job apply done by applicants  </p>
+          <h2 class="card-title text-xl text-black"> ScreenShot-2 : Recruiter's Dashboard </h2>
+          <p className=' text-black'> Recruiter will post job circular and receive notifications of job apply done by applicants  </p>
         </div>
       </div>
       <div class="card bg-base-100 shadow-xl image-full">
         <figure> <img src="https://i.ibb.co/hYM5wb5/Screenshot-2022-07-21-173126.png" alt="Shoes" /> </figure>
         <div class="card-body">
-          <h2 class="card-title">ScreenShot-3 : More about the Dashboard !</h2>
-          <p> Recruiter can also monitor all of their posts and can track , regulate all of the applicants whom will apply their circular using our stored data of the applicants.</p>
+          <h2 class="card-title text-xl text-black">ScreenShot-3 : More about the Dashboard !</h2>
+          <p className=' text-black'> Recruiter can also monitor all of their posts and can track , regulate all of the applicants whom will apply their circular using our stored data of the applicants.</p>
         </div>
       </div>
       <div class="card bg-base-100 shadow-xl image-full">
         <figure><img src="https://i.ibb.co/Jjvszmt/Screenshot-2022-07-21-173712.png" alt="Shoes" /></figure>
         <div class="card-body">
-          <h2 class="card-title">ScreenShot-4 : Explore Section </h2>
-          <p> Applicant's will have a special page for seeing all job posts. </p>
+          <h2 class="card-title text-xl text-black">ScreenShot-4 : Explore Section </h2>
+          <p className=' text-black'> Applicant's will have a special page for seeing all job posts. </p>
         </div>
       </div>
 
 
     </div>
-
-
   );
 };
 
