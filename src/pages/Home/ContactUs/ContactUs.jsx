b99e2d2be2271641eae4d2c2473cbf8357b21331
nazmul162001 <nazmulprime162001@gmail.com>
2022-07-26 21:54:19 +0600

    fixed color combinatin

--- src/pages/Home/ContactUs/ContactUs.jsx
+++ src/pages/Home/ContactUs/ContactUs.jsx
@@ -4,7 +4,7 @@ import './ContactUs.css'
 
 const ContactUs = () => {
   return (
-    <section className='contact_us'>
+    <section className='contact_us my-24 '>
       <div className="contact_content">
         <div className="contact_title h-96 relative">
           <div className="absolute text-white title_heading text-center">
@@ -12,14 +12,14 @@ const ContactUs = () => {
             <p className='mt-2'>Want to get in touch? We'd love to hear you. Here's how you can reach us...</p>
           </div>
         </div>
-        <div className="contact_form grid grid-cols-1 md:grid-cols-2 justify-center mt-[-50px] relative z-10 mx-0 md:mx-16 gap-5">
-          <div className="call p-10 bg-white text-center">
+        <div className="contact_form grid grid-cols-1 md:grid-cols-2 justify-center mt-[-100px] relative z-10 mx-0 md:mx-16 gap-5">
+          <div className="call p-10 bg-base-100 shadow-2xl rounded text-center">
             <span><i class="ri-phone-fill text-5xl"></i> </span>
             <p className="text-sm font-bold my-3">Call To Talk Now</p>
             <p className='text-sm my-3'>Interested in Job Onboard Services? Just pick up the phone to chat with a member of our Job Onboard team.</p>
             <p className='text-blue-400'>+880 1700 000000</p>
           </div>
-          <div className="connect p-10 bg-white text-center">
+          <div className="connect p-10 bg-base-100 shadow-2xl rounded text-center">
           <span><i class="ri-discuss-line text-5xl"></i> </span>
             <p className="text-sm font-bold my-3">Contact Customer Support</p>
             <p className='text-sm my-3'>Sometimes you need a little help from your friends. Or a Job Onboard Support rep. Don't worry... we're here for you.</p>
