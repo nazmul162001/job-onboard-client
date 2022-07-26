6fa0727479ef1cb85c2c075ed626e10e4bd34e6f
nazmul162001 <nazmulprime162001@gmail.com>
2022-07-26 22:50:30 +0600

    fixed all issu

--- src/pages/Home/ContactUs/ContactUs.jsx
+++ src/pages/Home/ContactUs/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'remixicon/fonts/remixicon.css'
+import mail from '../../../assets/images/contact/mail.png'
 import './ContactUs.css'
 
 const ContactUs = () => {
@@ -14,13 +15,13 @@ const ContactUs = () => {
         </div>
         <div className="contact_form grid grid-cols-1 md:grid-cols-2 justify-center mt-[-100px] relative z-10 mx-0 md:mx-16 gap-5">
           <div className="call p-10 bg-base-100 shadow-2xl rounded text-center">
-            <span><i class="ri-phone-fill text-5xl"></i> </span>
+            <span><i class="ri-phone-fill text-5xl text-blue-300"></i> </span>
             <p className="text-sm font-bold my-3">Call To Talk Now</p>
             <p className='text-sm my-3'>Interested in Job Onboard Services? Just pick up the phone to chat with a member of our Job Onboard team.</p>
             <p className='text-blue-400'>+880 1700 000000</p>
           </div>
           <div className="connect p-10 bg-base-100 shadow-2xl rounded text-center">
-          <span><i class="ri-discuss-line text-5xl"></i> </span>
+          <div className='flex justify-center'> <img className='w-12 h-12' src={mail} alt="" /> </div>
             <p className="text-sm font-bold my-3">Contact Customer Support</p>
             <p className='text-sm my-3'>Sometimes you need a little help from your friends. Or a Job Onboard Support rep. Don't worry... we're here for you.</p>
             <button className='p-5 bg-orange-500 my-5 rounded-lg text-white text-sm'>Contact Support</button>
