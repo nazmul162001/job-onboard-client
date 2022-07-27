5677850977cc4a48bc2097e97242d88d79d686e0
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-07-28 00:08:28 +0600

    basic ui setup for why jobonboard

--- src/pages/Home/WhyJobOnboard/WhyJobOnboard.jsx
+++ src/pages/Home/WhyJobOnboard/WhyJobOnboard.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import  './WhyJobOnboard.css'
 
 const WhyJobOnboard = () => {
   return (
-    <div>
-      <h2>Why Job Onboard</h2>
+    <div className="py-8 bg-violet-100 px-16">
+      <h2 className='text-center text-3xl md:text-4xl font-bold'>Why Job Onboard</h2>
+      <div className="flex">
+        <div className="flex basis-1/4 flex-col justify-center items-start space-y-4 ">
+          <button
+            type="button"
+          >
+            Easy User Interface
+          </button>
+          <button
+            type="button"
+          >
+            New Hire Onboarding
+          </button>
+          <button
+            type="button"
+          >
+            Easy Hiring Process
+          </button>
+          <button
+            type="button"
+          >
+            Understand User Need
+          </button>
+          <button
+            type="button"
+          >
+            Employee time off tracking
+          </button>
+          <button
+            type="button"
+          >
+            Employee database
+          </button>
+        </div>
+        <div className='p-5 flex justify-center items-center basis-3/4'>
+          <img className="w-full rounded-lg h-4/5" src={'https://i.ibb.co/wJYHwyH/1.png'} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
