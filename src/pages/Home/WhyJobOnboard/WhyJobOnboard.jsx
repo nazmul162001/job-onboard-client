2f636afb252044c127315acf117540734505eef8
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-07-28 01:54:31 +0600

    fixed responsive issue

--- src/pages/Home/WhyJobOnboard/WhyJobOnboard.jsx
+++ src/pages/Home/WhyJobOnboard/WhyJobOnboard.jsx
@@ -1,28 +1,27 @@
 import React, { useState } from 'react';
-import  './WhyJobOnboard.css'
+import './WhyJobOnboard.css'
 
 const WhyJobOnboard = () => {
   const [image, setImage] = useState("https://i.ibb.co/wJYHwyH/1.png");
   const buttonUrl = [
-    {name: 'Easy User Interface' , url :'https://i.ibb.co/S6cndgG/2.png'} ,
-    {name: 'New Hire Onboarding' , url :'https://i.ibb.co/9wj2ndf/3.png'} ,
-    {name: 'Easy Hiring Process' , url :'https://i.ibb.co/wJYHwyH/1.png'} ,
-    {name: 'Understand User Need' , url :'https://i.ibb.co/bNW6wgR/4.png'} ,
-    {name: 'Employee time off tracking' , url :'https://i.ibb.co/SdZcS65/5.png'} ,
-    {name: 'Employee database' , url :'https://i.ibb.co/Z14G3sp/6.png'} ,
+    { name: 'Easy User Interface', url: 'https://i.ibb.co/S6cndgG/2.png' },
+    { name: 'New Hire Onboarding', url: 'https://i.ibb.co/9wj2ndf/3.png' },
+    { name: 'Easy Hiring Process', url: 'https://i.ibb.co/wJYHwyH/1.png' },
+    { name: 'Understand User Need', url: 'https://i.ibb.co/bNW6wgR/4.png' },
+    { name: 'Employee time off tracking', url: 'https://i.ibb.co/SdZcS65/5.png' },
+    { name: 'Employee database', url: 'https://i.ibb.co/Z14G3sp/6.png' },
   ]
 
   return (
     <div className="py-8 bg-violet-100 px-16">
       <h2 className='text-center text-3xl md:text-4xl font-bold pb-5'>Why Job Onboard ?</h2>
-      <div className="flex ">
-        
-        <div className="flex basis-1/4 flex-col justify-center items-start space-y-4 ">
-        {buttonUrl.map(button => <button onClick={() => setImage(button.url)} className={button.url === image && 'active'}>
-          {button.name}
-        </button>)}
+      <div className="grid grid-cols-3">
+        <div className=" space-y-4 flex flex-col justify-center">
+          {buttonUrl.map(button => <button onClick={() => setImage(button.url)} className={button.url === image && 'active'}>
+            {button.name}
+          </button>)}
         </div>
-        <div className='p-5 flex justify-center items-center basis-3/4'>
+        <div className='p-5 col-span-2'>
           <img className="w-full rounded-lg " src={image} alt="why-jobOnboard-img" />
         </div>
       </div>
