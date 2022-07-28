b7e9981ccc373720fce195aa92d206b568f337d1
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-07-29 01:23:11 +0600

    module css added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from "./pages/Login/Login/Login";
 import SignUp from "./pages/Login/SignUp/SignUp";
 import ResetPassword from "./pages/Login/ResetPassword/ResetPassword";
 import AboutUs from "./pages/AboutUs/AboutUs";
+import Jobs from "./pages/Jobs/Jobs";
 
 function App() {
   const [theme, setTheme] = useState(false);
@@ -27,6 +28,7 @@ function App() {
       <Navbar handleThemeChange={handleThemeChange} theme={theme} />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/team" element={<Team />} />
         <Route path="/login" element={<Login />} />
