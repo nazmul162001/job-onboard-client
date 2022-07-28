21d8c2d286b498a717903f63abe64352d7197963
@ ANTU <sajolhowlader08@gmail.com>
2022-07-28 16:32:59 +0600

    add hunterImg

--- src/pages/Home/ForJobHunter/JobHunter.jsx
+++ src/pages/Home/ForJobHunter/JobHunter.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import hunterImgOne from "../../../assets/images/JobHunter/1.png";
-import hunterImgTwo from "../../../assets/images/JobHunter/2.png";
-import hunterImgThree from "../../../assets/images/JobHunter/3.png";
-import hunterImgFour from "../../../assets/images/JobHunter/4.png";
 import "./JobHunter.css";
 const JobHunter = () => {
   return (
     <section>
-      <h2 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold opacity-70 pt-8 lg:pt-0 mb-10">
+      <h2 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold opacity-70 pt-8 lg:pt-0 ">
         For Job Hunter
       </h2>
       <div className="jobHunterContainer grid grid-cols-4 gap-8">
-      <div className="jobHunterInfoContainer">
-          <div className="jobHunterImg">
-            <img className="max-w-full" src={hunterImgOne} alt="" />
+        <div className="jobHunterInfoContainer">
+          <div class="avatar hunterImg">
+            <div class="lg:w-48 sm:w-full rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img
+                className="max-w-full"
+                src={hunterImgOne}
+                alt="Easy to Apply"
+              />
+            </div>
           </div>
           <div className="hunterInfo">
             <h4>Easy To Apply</h4>
@@ -23,7 +26,60 @@ const JobHunter = () => {
             </p>
           </div>
         </div>
-        
+        <div className="jobHunterInfoContainer">
+          <div class="avatar hunterImg">
+            <div class="lg:w-48 sm:w-full rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img
+                className="max-w-full"
+                src={hunterImgOne}
+                alt="Easy to Apply"
+              />
+            </div>
+          </div>
+          <div className="hunterInfo">
+            <h4>Easy To Apply</h4>
+            <p>
+              Easy apply any kind of jobs.Find your best jobs acording to to
+              your skill
+            </p>
+          </div>
+        </div>
+        <div className="jobHunterInfoContainer">
+          <div class="avatar hunterImg">
+            <div class="lg:w-48 sm:w-full rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img
+                className="max-w-full"
+                src={hunterImgOne}
+                alt="Easy to Apply"
+              />
+            </div>
+          </div>
+          <div className="hunterInfo">
+            <h4>Easy To Apply</h4>
+            <p>
+              Easy apply any kind of jobs.Find your best jobs acording to to
+              your skill
+            </p>
+          </div>
+        </div>
+        <div className="jobHunterInfoContainer">
+          <div class="avatar hunterImg">
+            <div class="lg:w-48 sm:w-full rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img
+                className="max-w-full"
+                src={hunterImgOne}
+                alt="Easy to Apply"
+              />
+            </div>
+          </div>
+          <div className="hunterInfo">
+            <h4>Easy To Apply</h4>
+            <p>
+              Easy apply any kind of jobs.Find your best jobs acording to to
+              your skill
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
