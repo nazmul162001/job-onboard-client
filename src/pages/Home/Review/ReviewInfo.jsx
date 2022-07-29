da58c5ca7d4863b686a2b307ea7dc80056556b31
nazmul162001 <nazmulprime162001@gmail.com>
2022-07-29 08:28:49 +0600

    added review content & custom style

--- src/pages/Home/Review/ReviewInfo.jsx
+++ src/pages/Home/Review/ReviewInfo.jsx
@@ -6,8 +6,20 @@ const ReviewInfo = ({review}) => {
   
   
   return (
-    <div>
-      <h2 className='border-2 border-opacity-30 h-60 border-orange-400 rounded p-5 '>{reviewDetails} </h2>
+    <div className='border-2 border-opacity-30 border-orange-400 rounded p-5 min-h-70 '>
+      <p className='pb-5'>{reviewDetails.slice(0, 200)} </p>
+      <div className="empty my-4 w-4/5 mx-auto h-[1px] bg-gray-500 opacity-30"></div>
+      <div className="profile flex items-center">
+      <div class="avatar">
+        <div class="w-12 rounded-full">
+          <img src="https://placeimg.com/192/192/people" alt=''/>
+        </div>
+      </div>
+        <div className="profile_name pl-2 text-left">
+         <p className='text-sm'>{name}</p>
+         <p className='text-sm'>{location}</p>
+        </div>
+      </div>
     </div>
   );
 };
