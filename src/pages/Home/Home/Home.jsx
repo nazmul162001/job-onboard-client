a3ed1140c8b0ba991beb01a88740a35a7c31c2ad
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-07-31 13:43:19 +0600

    deploy site

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,6 +5,7 @@ import Branding from "../Branding/Branding";
 import ContactUs from "../ContactUs/ContactUs";
 import ForEmployers from "../ForEmployers/ForEmployers";
 import JobHunter from "../ForJobHunter/JobHunter";
+import Review from "../Review/Review";
 import WhyChooseUs from "../WhyChooseUs/WhyChooseUs";
 import WhyJobOnboard from "../WhyJobOnboard/WhyJobOnboard";
 
