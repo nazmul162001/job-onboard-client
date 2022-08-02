9ad413b1c6f38b1700d867cdeff45916c477a29e
nazmul162001 <nazmulprime162001@gmail.com>
2022-08-02 22:03:42 +0600

    added dashboard all section template

--- src/pages/Dashboard/WelcomeDashboard/WelcomeDashboard.jsx
+++ src/pages/Dashboard/WelcomeDashboard/WelcomeDashboard.jsx
@@ -18,7 +18,7 @@ const WelcomeDashboard = () => {
             {/* main dashboard  */}
             <div className="content_left">
             <div className="dashboard_route h-72 bg-white m-5">
-              <h2 className="p-5 text-xl font-bold">Interview for me</h2>
+              <h2 className="p-5 text-xl">Interview for me</h2>
             </div>
               <DashboardReferrals />
               <DashboardCelebration />
