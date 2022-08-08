54729e0edc2a77a8968827ee5b8de3f4aae87c80
@ ANTU <sajolhowlader08@gmail.com>
2022-08-08 20:01:12 +0600

    add employee modal

--- src/pages/ManageDashboard/EmployeeRoot/AddEmployee.jsx
+++ src/pages/ManageDashboard/EmployeeRoot/AddEmployee.jsx
@@ -1,23 +1,190 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { BsPersonPlusFill } from "react-icons/bs";
 const AddEmployee = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
   return (
     <div>
+      <input
+        type="checkbox"
+        id="add-new-employee-modal"
+        className="modal-toggle "
+      />
+      <div className="modal ">
+        <div className="modal-box lg:w-10/12 lg:max-w-2xl">
+          <label
+            for="my-modal-3"
+            class="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
 
-<input type="checkbox" id="add-new-employee-modal" class="modal-toggle" />
-<div class="modal modal-bottom sm:modal-middle">
-  <div class="modal-box">
-    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-    <div class="modal-action">
-      <label for="add-new-employee-modal" class="btn">Yay!</label>
-    </div>
-  </div>
-</div>
+          <div>
+            <form className="space-y-2">
+              {/* name filed  */}
+              <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
+                <div className="flex flex-col space-y-1 gap-y-1">
+                  <label className="text-lg pl-2">
+                    Full Name <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter Full Name"
+                    className="border rounded-lg py-1 text-lg pl-3 "
+                    {...register("fullName", {
+                      required: {
+                        value: true,
+                        message: "This field is required",
+                      },
+                    })}
+                  />
+                  <p className="text-[13px] text-red-500 pl-3">
+                    {errors.fullName?.message}
+                  </p>
+                </div>
+                <div className="flex flex-col space-y-1 gap-y-1">
+                  <label className="text-lg pl-2">
+                    Employe ID <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="Number"
+                    placeholder="Enter ID No"
+                    className="border rounded-lg py-1 text-lg pl-3 "
+                    {...register("fullName", {
+                      required: {
+                        value: true,
+                        message: "This field is required",
+                      },
+                    })}
+                  />
+                  <p className="text-[13px] text-red-500 pl-3">
+                    {errors.fullName?.message}
+                  </p>
+                </div>
+              </div>
+              {/* name filed end */}
 
+              <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
+                <div className="flex flex-col space-y-1 gap-y-1">
+                  <label className="text-lg pl-2">
+                    Email<span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    className="border rounded-lg py-1 text-lg pl-3 "
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "This field is required",
+                      },
+                    })}
+                  />
+                  <p className="text-[13px] text-red-500 pl-3">
+                    {errors.email?.message}
+                  </p>
+                </div>
 
+                <div className="flex flex-col space-y-1 gap-y-1">
+                  <label className="text-lg pl-2">
+                    Contact number <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="Phone number"
+                    className="border rounded-lg  py-1 text-lg pl-3 "
+                    {...register("phoneNumber", {
+                      required: {
+                        value: true,
+                        message: "This field is required",
+                      },
+                    })}
+                  />
+                  <p className="text-[13px] text-red-500 pl-3">
+                    {errors.phoneNumber?.message}
+                  </p>
+                </div>
+              </div>
 
-      <label for="add-new-employee-modal" className="mr-5 flex items-center bg-base-300 py-2 px-3 rounded-md font-bold cursor-pointer">
+              {/* social link  */}
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                <div className="flex flex-col space-y-1 gap-y-1">
+                  <input
+                    type="text"
+                    placeholder="Portfolio"
+                    className="border rounded-lg py-1 text-lg pl-3 "
+                    {...register("portfolio", {
+                      required: {
+                        value: true,
+                        message: "This field is required",
+                      },
+                    })}
+                  />
+                  <p className="text-[13px] text-red-500 pl-3">
+                    {errors.portfolio?.message}
+                  </p>
+                </div>
+
+                <div className="flex flex-col space-y-1 gap-y-1">
+                  <input
+                    type="text"
+                    placeholder="Linkedin"
+                    className="border rounded-lg py-1 text-lg pl-3 "
+                    {...register("linkedin", {
+                      required: {
+                        value: true,
+                        message: "This field is required",
+                      },
+                    })}
+                  />
+                  <p className="text-[13px] text-red-500 pl-3">
+                    {errors.linkedin?.message}
+                  </p>
+                </div>
+              </div>
+
+              {/* social link end */}
+
+              <div className="flex flex-col space-y-1 gap-y-1 py-5">
+                <h4 className="pl-2 md:text-lg">
+                  Why should you be hired for this role?{" "}
+                  <span className="text-red-500">*</span>
+                </h4>
+                <textarea
+                  type="text"
+                  rows={4}
+                  placeholder="Add a cover letter"
+                  className="border rounded-lg py-1 text-xl pl-3 "
+                  {...register("coverLetter", {
+                    required: {
+                      value: true,
+                      message: "This field is required",
+                    },
+                  })}
+                />
+                <p className="text-[13px] text-red-500 pl-3"></p>
+              </div>
+
+              <div className="pb-5 lg:pb-2 text-center lg:text-start">
+                <button className="px-5 py-3  border bg-primary rounded-lg text-lg  text-white">
+                  Submit Application
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <label
+        for="add-new-employee-modal"
+        className="mr-5 flex items-center bg-base-300 py-2 px-3 rounded-md font-bold cursor-pointer"
+      >
         <BsPersonPlusFill /> Add New
       </label>
     </div>
