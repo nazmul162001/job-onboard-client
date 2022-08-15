977c3528f185042bf85cea6dad809b2d69699b0d
@ ANTU <sajolhowlader08@gmail.com>
2022-08-15 21:33:56 +0600

    add new delete

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
   return (
     <div>
-      <h2>This is blog page</h2>
+      <div className="titleContainer flex flex-col text-center my-4">
+        <h2 className="text-center text-2xl font-bold ">
+          <span className="text-4xl font-serif text-primary">W</span>elcome To
+          Our Blog
+        </h2>
+
+        <span className="bg-primary w-48 h-1 mx-auto mt-2"></span>
+
+        <div className="firstBlogContainer">
+          <div className="firstBlogInfo">
+
+
+          </div>
+          <div className="secondBlogInfo">
+
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
