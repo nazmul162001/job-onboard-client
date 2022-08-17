d8e46e731f080e1bbef7ab815751b0604a1e1685
@ ANTU <sajolhowlader08@gmail.com>
2022-08-17 12:49:31 +0600

    add responsive

--- src/pages/Blog/BlogsDetail.jsx
+++ src/pages/Blog/BlogsDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BASE_API } from "../../config";
+import Footer from "../../Shared/Footer/Footer";
 const BlogsDetail = () => {
   const [singleBlogs, setSingleBlog] = useState([]);
   const { blogId } = useParams();
@@ -15,8 +16,8 @@ const BlogsDetail = () => {
   const { title, image, about } = singleBlogs;
 
   return (
-    <section className="bg-red-50">
-      <div className="container mx-auto px-10">
+    <section className="bg-base-400">
+      <div className="container mx-auto px-3 lg:px-10">
         <div className=" border-b-2 border-cyan-600 py-3">
           <h2 className="text-center text-2xl font-bold ">
             <span className="text-5xl font-serif text-primary">
@@ -27,17 +28,18 @@ const BlogsDetail = () => {
         </div>
 
         <div className="singleBlogDetails py-10">
-          <div className="titleAndImg grid items-center gap-5">
-            <h2 className="text-4xl font-bold leading-[3rem]">{title}</h2>
+          <div className="titleAndImg grid items-center gap-5 ">
+            <h2 className="text-4xl font-bold leading-[3rem] ">{title}</h2>
             <img
               className="rounded-lg w-full h-80 object-cover max-w-full"
               src={image}
               alt=""
             />
           </div>
-          <p className="py-10 text-xl font-normal text-stone-900">{about}</p>
+          <p className="py-28 text-xl font-normal text-stone-900">{about}</p>
         </div>
       </div>
+      <Footer />
     </section>
   );
 };
