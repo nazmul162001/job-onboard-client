d6a4434deccc07aa78fc56346766027b676e8b9e
nazmul162001 <nazmulprime162001@gmail.com>
2022-08-18 06:58:32 +0600

    added tracking header

--- src/pages/Features/ApplicantTracking/ApplicantTracking.jsx
+++ src/pages/Features/ApplicantTracking/ApplicantTracking.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const ApplicantTracking = () => {
   return (
-    <div>
-      <h2>This is Applicant Tracking page </h2>
-    </div>
+    <>
+    <section className='applicants_tracking bg-[#FFFFFF]'>
+      <div className="tracking text-center my-20 mx-52 text-black">
+        <h1 className="text-5xl font-extrabold my-3">Modern Applicants Tracking Software</h1>
+        <h3 className="text-xl my-3">Start with a 21-days free trial of Job OnBoard applicant tracking software, pick any plan. Decide later if you want to upgrade or continue with the free version.</h3>
+        <button className='my-4 px-3 py-2 bg-indigo-600 text-white font-bold rounded'>GET YOUR FREE ATS ACCOUNT</button>
+      </div>
+    </section>
+    </>
   );
 };
 
