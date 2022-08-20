0afaddf4b7c96c5b6e795a9719d2899c3eabf289
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-08-21 00:40:15 +0600

    deploy client

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { toast } from "react-hot-toast";
 import auth from "../../Auth/Firebase/Firebase.init";
 import logo from "../../Pages/Assets/logo/logo.png";
 import { InitializeContext } from "../../App";
-import { FiChevronDown, FiLogOut } from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import useAdmin from "../../Hooks/useAdmin";
 import useHrManager from "../../Hooks/useHrManager";
