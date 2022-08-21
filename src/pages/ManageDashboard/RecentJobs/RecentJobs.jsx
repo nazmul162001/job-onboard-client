675916a396db9ef30f684330a72bdb5183cba9fe
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-08-21 14:28:36 +0600

    row handle

--- src/pages/ManageDashboard/RecentJobs/RecentJobs.jsx
+++ src/pages/ManageDashboard/RecentJobs/RecentJobs.jsx
@@ -5,9 +5,9 @@ import auth from '../../../Auth/Firebase/Firebase.init';
 import axios from 'axios';
 import { BASE_API } from '../../../config';
 
-const RecentJobs = ({ myJob,index }) => {
+const RecentJobs = ({ myJob, index }) => {
 
-  const { data} = useQuery(["count", auth, myJob?.createdDate], () => axios.get(`${BASE_API}/applicants/appliedCandidate?email=${auth?.currentUser?.email}&createdDate=${myJob?.createdDate}`,
+  const { data } = useQuery(["count", auth, myJob?.createdDate], () => axios.get(`${BASE_API}/applicants/appliedCandidate?email=${auth?.currentUser?.email}&createdDate=${myJob?.createdDate}`,
     {
       headers: {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
@@ -15,18 +15,19 @@ const RecentJobs = ({ myJob,index }) => {
     }))
 
   // console.log(data);
-  const countData = data?.data
+  const countApplicant = data?.data
+  console.log(countApplicant);
 
   return (
-    <tr class="bg-white text-center border-b transition duration-300 ease-in-out hover:bg-gray-100">
-      <th>{index + 1}.</th>
-        <td>{myJob.jobTitle}</td>
-        <td>{moment(myJob?.createdDate).format("MMMM DD, YYYY")}</td>
-        <td>
-          ${myJob.salary} <small>/m</small>
-        </td>
-        <td>{myJob?.location}</td>
-        <td>{countData?.length}</td>
+    <tr class="bg-white text-center border-b transition duration-300 ease-in-out hover:bg-gray-100 py-5">
+      <th className='py-3'>{index + 1}.</th>
+      <td className='py-3'>{myJob.jobTitle}</td>
+      <td className='py-3'>{moment(myJob?.createdDate).format("MMMM DD, YYYY")}</td>
+      <td className='py-3'>
+        ${myJob.salary} <small>/m</small>
+      </td>
+      <td className='capitalize '>{myJob?.location}</td>
+      <td className='font-semibold font-mono '>{countApplicant?.length ? countApplicant?.length : 'No Applicant'}</td>
     </tr>
   );
 };
