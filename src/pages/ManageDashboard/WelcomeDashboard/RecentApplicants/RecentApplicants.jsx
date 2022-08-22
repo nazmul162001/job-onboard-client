e756292cf7d3a5b89b45fb944b7721b4bd27a37d
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-08-22 16:39:21 +0600

    card style done

--- src/pages/ManageDashboard/WelcomeDashboard/RecentApplicants/RecentApplicants.jsx
+++ src/pages/ManageDashboard/WelcomeDashboard/RecentApplicants/RecentApplicants.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const RecentApplicants = ({  revApplicant }) => {
+const RecentApplicants = ({ revApplicant }) => {
   console.log(revApplicant?.profileUrl)
-  const {profileUrl , displayName , jobTitle}  = revApplicant 
+  const { profileUrl, displayName, jobTitle } = revApplicant
   return (
-    <div className='shadow-lg hover:shadow-2xl p-5 text-center space-y-4 border rounded-lg'>
-      <div class="avatar py-2 mx-auto">
+    <div className='shadow-lg hover:shadow-2xl p-5 text-center space-y-4 border rounded-lg '>
+
+      <div class="avatar py-2 mx-auto relative">
+        <label class="absolute left-[-70px] top-2 text[-11px] border px-2  rounded-xl">Applied </label>
         <div class="w-16 ring-2 rounded-full ">
           {profileUrl ? (
             <img src={profileUrl} alt="candidate" />
@@ -15,7 +17,8 @@ const RecentApplicants = ({  revApplicant }) => {
         </div>
       </div>
       <h2 className="font-semibold">{displayName}</h2>
-      <h3 className="font-mono text-lg font-semibold">{jobTitle}</h3>
+      <h3 className="text-[15px]">{jobTitle}</h3>
+      <button className='btn btn-sm btn-outline text-[12px] text-secondary '>View Details</button>
     </div>
   );
 };
