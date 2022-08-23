2fcf7614f7dad36dc5b736195509055925cf91e8
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-08-23 15:59:59 +0600

    changes on dashboard

--- src/pages/ManageDashboard/Dashboard/Dashboard.jsx
+++ src/pages/ManageDashboard/Dashboard/Dashboard.jsx
@@ -4,7 +4,6 @@ import React, { useContext, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { toast } from "react-hot-toast";
 import { AiOutlinePlus } from "react-icons/ai";
-import { BsGrid } from "react-icons/bs";
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
 import { InitializeContext } from "../../../App";
 import auth from "../../../Auth/Firebase/Firebase.init";
@@ -12,9 +11,10 @@ import Loader from "../../../Components/Loader/Loader";
 import { BASE_API } from "../../../config";
 import useAdmin from "../../../Hooks/useAdmin";
 import useHrManager from "../../../Hooks/useHrManager";
-import logo from "../../Assets/logo/logo.png";
+import Logo from "../../Assets/logo/logo.png";
 import ReactTooltip from 'react-tooltip';
 import "./Dashboard.css";
+import { FiLogOut } from "react-icons/fi";
 
 const Dashboard = ({children}) => {
   const { handleThemeChange, theme, profileUrl } =
@@ -73,13 +73,13 @@ const Dashboard = ({children}) => {
   }
 
   return (
-    <div className="container">
+    <div className="">
       <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
       <div className={`drawer-content p-3 md:p-3 ${open ? 'ml-72' : 'ml-24'}`}>
         <div className="header z-50 sticky top-0 flex justify-between items-center bg-base-300 p-4 rounded-lg">
-          <div className="md:hidden">
-            {/* empty area*/}
-          </div>
+          {/* <div className="md:hidden">
+            empty area
+          </div> */}
           <span className="font-semibold text-xl hidden md:flex justify-center items-center gap-1">
             Welcome,{" "}
             <div className="text-primary flex justify-center items-center">
@@ -105,7 +105,7 @@ const Dashboard = ({children}) => {
               to="/"
               className={`text-lg lg:text-2xl md:text-2xl font-semibold block md:hidden`}
             >
-              <img src={logo} alt="" className="w-24" />
+              <img src={Logo} alt="" className="w-24" />
             </Link>
           )}
           <div className="flex justify-center items-center gap-8">
@@ -165,6 +165,51 @@ const Dashboard = ({children}) => {
                   )}
                 </div>
               </label>
+              <ul
+                tabIndex="0"
+                className="mt-3 p-2 shadow-lg menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+              >
+                {!admin && !hr && (
+                  <li>
+                    <Link
+                      to="/dashboard/profile"
+                      className="py-3 font-semibold"
+                    >
+                      Profile
+                    </Link>
+                  </li>
+                )}
+                <li className="font-semibold md:hidden">
+                  <button
+                    onClick={handleThemeChange}
+                    className="rounded-full lg:mx-2 font-bold"
+                  >
+                    {theme ? (
+                      <svg
+                        className="swap-on fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                      </svg>
+                    ) : (
+                      <svg
+                        className="swap-off fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                      </svg>
+                    )}
+                  </button>
+                </li>
+                <li className="font-semibold">
+                  <button onClick={handleLogOut}>
+                    <FiLogOut />
+                    Logout
+                  </button>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
@@ -177,33 +222,33 @@ const Dashboard = ({children}) => {
         <div
           className={` ${
             open ? "w-72" : "w-20 "
-          } bg-[#081A51] h-screen p-5  pt-8 relative duration-300`}
+          } bg-primary h-screen p-5  pt-8 relative duration-300`}
         >
           <img
             src="./sidebar/control.png"
             alt="control"
-            className={`absolute cursor-pointer -right-3 top-9 w-7 border-[#081A51]
+            className={`absolute cursor-pointer -right-3 top-9 w-7 border-primary
            border-2 rounded-full  ${!open && "rotate-180"}`}
             onClick={() => setOpen(!open)}
           />
           <div
-            className="flex gap-x-4 items-center"
+            className="flex items-center justify-center"
             onClick={() => navigate("/")}
           >
             <img
-              src="./sidebar/logo.png"
+              src={Logo}
               alt=""
-              className={`cursor-pointer duration-500 ${
+              className={`cursor-pointer duration-500 w-24 ${
                 open && "rotate-[360deg]"
               }`}
             />
-            <h1
+            {/* <h1
               className={`text-white origin-left font-medium text-xl duration-200 ${
                 !open && "scale-0"
               }`}
             >
               OnBoard
-            </h1>
+            </h1> */}
           </div>
 
           {/* Admin Dashboard  */}
