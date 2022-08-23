56d4bcaca438b38fe6d8713f41d0b2e57db6f8e8
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-08-23 12:11:45 +0600

    applied job hook use for dashboard

--- src/pages/ManageDashboard/WelcomeDashboard/CandidateDashboard/CandidateDashboard.jsx
+++ src/pages/ManageDashboard/WelcomeDashboard/CandidateDashboard/CandidateDashboard.jsx
@@ -3,7 +3,133 @@ import React from 'react';
 const CandidateDashboard = () => {
   return (
     <div>
-      <h2>This is candidate dashboard</h2>
+      <div className="">
+        <section className="h-full main_dashboard static z-10 ">
+        
+          <div className="">
+            <div className="dashboard_route bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-3">
+              <div className="card_content my-5 flex bg-orange-100 bg-opacity-60 py-2 rounded">
+                <div className="icon p-5">
+                  <i class="ri-group-line text-white text-2xl rounded p-5 bg-rose-400"></i>
+                </div>
+                <div className="card_details text-black">
+                  {/* <h2 className="font-bold text-xl ">{getApplicants ? getApplicants?.length : 0}</h2> */}
+                  <p className="text-[14px]">Applied Job</p>
+                </div>
+              </div>
+              <div className="card_content my-5 flex bg-orange-100 bg-opacity-60 py-2 rounded">
+                <div className="icon p-5">
+                  <i class="ri-briefcase-line text-white text-2xl rounded p-5 bg-pink-500"></i>
+                </div>
+                <div className="card_details text-black">
+                  <h2 className="font-bold text-xl">0</h2>
+                  <p className="text-[14px]">Unfinish Job</p>
+                </div>
+              </div>
+              <div className="card_content my-5 flex bg-orange-100 bg-opacity-60 py-2 rounded">
+                <div className="icon p-5">
+                  <i class="ri-briefcase-line text-white text-2xl rounded p-5 bg-orange-400"></i>
+                </div>
+                <div className="card_details text-black">
+                  <h2 className="font-bold text-xl">0</h2>
+                  <p className="text-[14px]">New message</p>
+                </div>
+              </div>
+            </div>
+            
+
+            {/* Recent Applied Job */}
+            <h2 className="mt-5 mb-3 lg:pl-4 font-bold">Recent Applied Job</h2>
+            {/* {revGetApplicants?.length > 0 ? (
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                {revGetApplicants.map((revApplicant, index) => (
+                  <RecentApplication
+                    key={index}
+                    revApplicant={revApplicant}
+                  />
+                ))}
+              </div>
+            ) : (
+              <div className="recent-application p-4 bg-base-200 shadow rounded ">
+                <p className='text-red-500'>No Applicants Found</p>
+              </div>
+            )}
+
+            {/* Recent Jobs  */}
+            <h2 className="mt-5 mb-3 lg:pl-4 font-bold">Recent Jobs</h2>
+            {/* {revMyJob?.length > 0 ? (
+              <div class="flex flex-col">
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="overflow-hidden">
+                      <table class="min-w-full">
+                        <thead class="border-b bg-primary ">
+                          <tr className="text-center">
+                            <th
+                              class="text-sm font-medium text-white px-6 py-4 "
+                              scope="col">No</th>
+                            <th
+                              scope="col"
+                              class="text-sm font-medium text-white px-6 py-4 "
+                            >
+                              Job Title
+                            </th>
+                            <th
+                              scope="col"
+                              class="text-sm font-medium text-white px-6 py-4 "
+                            >
+                              Posted Date
+                            </th>
+                            <th
+                              scope="col"
+                              class="text-sm font-medium text-white px-6 py-4 "
+                            >
+                              Salary
+                            </th>
+                            <th
+                              scope="col"
+                              class="text-sm font-medium text-white px-6 py-4 "
+                            >
+                              Location
+                            </th>
+                            <th
+                              scope="col"
+                              class="text-sm text-left font-medium text-white px-6 py-4 "
+                            >
+                              Applicants
+                            </th>
+                            <th
+                              scope="col"
+                              class="text-sm  font-medium text-white px-6 py-4 "
+                            >
+                              Action
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {revMyJob?.map((myJob, index) => (
+                            <RecentJobs
+                              key={index}
+                              myJob={myJob}
+                              index={index}
+                            />
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="recent-application p-4 bg-base-200 shadow  rounded ">
+                <p className='text-red-500'>No Jobs Found</p>
+              </div>
+            )}  */}
+
+            
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
