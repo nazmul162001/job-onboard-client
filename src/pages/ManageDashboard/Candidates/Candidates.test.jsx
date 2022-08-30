7ebef1dfebf6c50b94fb7df517e7b7224d617fdc
Arefin <arefin.khan8364@gmail.com>
2022-08-31 00:06:01 +0600

    candidate details button tested

--- src/pages/ManageDashboard/Candidates/Candidates.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import Candidates from "./Candidates"
-
-test('renders Manage Candidates', () => {
-    render(<Candidates />);
-    const linkElement = screen.getByText(/Manage Candidates/i);
-    expect(linkElement).toBeInTheDocument();
-});
