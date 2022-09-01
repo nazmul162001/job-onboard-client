d3b7efd94c9fd1af12be905be95c47926f809e2b
@ ANTU <sajolhowlader08@gmail.com>
2022-09-01 13:20:11 +0600

    resolve dashboard error

--- src/pages/ManageDashboard/WelcomeDashboard/WelcomeDashboard.jsx
+++ src/pages/ManageDashboard/WelcomeDashboard/WelcomeDashboard.jsx
@@ -4,15 +4,14 @@ import { useDispatch, useSelector } from "react-redux";
 import auth from "../../../Auth/Firebase/Firebase.init";
 import Loading from "../../../Components/Loading/Loading";
 import { fetchAllEmployeDetails } from "../../../Features/AllEmployeDetails/AllEmployeDetailsSlice";
+import { fetchHrJobs } from "../../../Features/HrJobs/HrJobsSlice";
 import useAdmin from "../../../Hooks/useAdmin";
 import useCandidate from "../../../Hooks/useCandidate";
-import useHrJob from "../../../Hooks/useHrJob";
 import useHrManager from "../../../Hooks/useHrManager";
 import useTitle from "../../../Hooks/useTitle";
 import AdminDashboard from "./AdminDashboard/AdminDashboard";
 import CandidateDashboard from "./CandidateDashboard/CandidateDashboard";
 import HrDashboard from "./HrDashboard/HrDashboard";
-
 
 const WelcomeDashboard = () => {
   useTitle("Dashboard");
@@ -20,7 +19,6 @@ const WelcomeDashboard = () => {
   const [admin, adminLoading] = useAdmin(user);
   const [hr, hrLoading] = useHrManager(user);
   // console.log(hr);
-
 
   // const { data } = useEmployeeInfo();
   // const allEmployeDetails = data?.data;
@@ -30,15 +28,18 @@ const WelcomeDashboard = () => {
     dispatch(fetchAllEmployeDetails());
   }, [dispatch]);
 
-
-
   // const { data } = useEmployeeInfo();
   // const allEmployeDetails = data?.data;
 
+  // const [hrJobs] = useHrJob();
 
-  const [hrJobs] = useHrJob();
+  const { hrJobs } = useSelector((state) => state.hrJobs);
+
+  useEffect(() => {
+    dispatch(fetchHrJobs());
+  }, [dispatch]);
+
   let revMyJob = [].concat(hrJobs).reverse().slice(0, 3);
-  // console.log(revMyJob);
 
   const { getApplicants } = useCandidate();
   const revGetApplicants = [].concat(getApplicants).reverse().slice(0, 4);
