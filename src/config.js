bd75e4710d04126d6f6a8c7412cc481fcf58d11b
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-09-02 18:20:14 +0600

    updated minor bugs

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_API = "http://localhost:5000";
-// export const BASE_API = "https://job-onboard.herokuapp.com";
+// export const BASE_API = "http://localhost:5000";
+export const BASE_API = "https://job-onboard.herokuapp.com";
