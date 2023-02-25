cb19166f823b2b5cd0a9adae4607af806ddf4a3d
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2023-02-25 19:32:24 +0600

    update firebase env file

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const BASE_API = "http://localhost:5000";
+// export const BASE_API = "http://localhost:5000";
 // export const BASE_API = "https://job-onboard.herokuapp.com";
+export const BASE_API = "https://job-onboard-server-production.up.railway.app";
